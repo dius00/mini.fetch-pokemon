49f213513ea4a944ed8b57253292824d7601d0d7
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-10-09 13:27:21 +0900

    Completed findNames using async/await, includes a catch method
    Co-Authored-By: Yoshiaki Aibara <69150682+Yoshi106@users.noreply.github.com>

--- pokemonager.js
+++ pokemonager.js
@@ -1,10 +1,17 @@
+//const fetch = require('node-fetch');
+
 (() => {
   class Pokemonager {
     // This should return an array of all the names of n Pokemon from the Pokemon API.
-    findNames(n) {
-      // Your code here.
+    async findNames(n) {
+      try{
+        //fetch(`https://pokeapi.co/api/v2/pokemon/1`).then((value) => value.json()).then((test) => console.log(test));
+        let { results } = await (await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${n}`)).json();
+        return results.map((value) => value.name);
+      } catch (error) {
+          console.error(error);
+      }
     }
-
     // This should return an array of all the Pokemon that are under a particular weight.
 
     findUnderWeight(weight) {
@@ -15,4 +22,7 @@
   }
 
   window.Pokemonager = Pokemonager;
+
+  //let test = new Pokemonager();
+  //test.findNames(3);
 })();
