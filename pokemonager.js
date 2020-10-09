c67a40943a39c23fc994312eb48503d3b9ee1d1d
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-10-09 14:28:03 +0900

    completed findUnderWeight
    NOTE: Array built in methods will not work for async code.
    Co-Authored-By: Yoshiaki Aibara <69150682+Yoshi106@users.noreply.github.com>

--- pokemonager.js
+++ pokemonager.js
@@ -5,24 +5,26 @@
     // This should return an array of all the names of n Pokemon from the Pokemon API.
     async findNames(n) {
       try{
-        //fetch(`https://pokeapi.co/api/v2/pokemon/1`).then((value) => value.json()).then((test) => console.log(test));
         let { results } = await (await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${n}`)).json();
         return results.map((value) => value.name);
       } catch (error) {
           console.error(error);
       }
     }
-    // This should return an array of all the Pokemon that are under a particular weight.
 
-    findUnderWeight(weight) {
-      // Your code here.
-      // ** LIMIT TO THE FIRST 10 POKEMON
-      // We don't want to make too many unnecessary calls to the Pokemon API
+    // This should return an array of all the Pokemon that are under a particular weight.
+    async findUnderWeight(weight) {
+      try{
+        let { results } = await (await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10`)).json();
+        const pokemons = [];
+         for(let value of results) {
+             pokemons.push( await (await fetch(value.url)).json());
+         }
+         return pokemons.filter((pokemon) => pokemon.weight < weight)
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
-
   window.Pokemonager = Pokemonager;
-
-  //let test = new Pokemonager();
-  //test.findNames(3);
 })();
